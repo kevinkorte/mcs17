f55786606d9d2945c9683864f4400eddc2d64660
Kevin Korte <kevinkorte15@gmail.com>
2017-10-16 10:39:32 -0700

    commit from last week never made

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -4,6 +4,7 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/new/new.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
@@ -12,6 +13,12 @@ FlowRouter.route('/', {
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
   },
+});
+FlowRouter.route('/new', {
+  name: 'App.new',
+  action() {
+    BlazeLayout.render('App_body', {main: 'App_new'});
+  }
 });
 
 FlowRouter.notFound = {
